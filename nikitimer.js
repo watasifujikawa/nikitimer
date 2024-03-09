19cd922f08a1fe280eb7ae2f008eb6190aef5de6
fuji <115616515+watasifujikawa@users.noreply.github.com>
2024-03-09 20:51:22 +0900

    newcommit
    初webコミット

---
+++ nikitimer.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+alert("誕生日まで");
+function showRestTime() {
+  const now = new Date();
+  const goal = new Date(2050, 0, 1);
+
+  const restMillisecond = goal.getTime() - now.getTime();
+  const day = Math.floor(restMillisecond / 1000 / 60 / 60 / 24);
+  const hour = Math.floor(restMillisecond / 1000 / 60 / 60) % 24;
+  const minute = Math.floor(restMillisecond / 1000 / 60) % 60;
+  const second = Math.floor(restMillisecond / 1000) % 60;
+
+  document.getElementById('day').textContent = day;
+  document.getElementById('hour').textContent = hour;
+  document.getElementById('minute').textContent = String(minute).padStart(2, '0');
+  document.getElementById('second').textContent = String(second).padStart(2, '0');
+}
+
+setInterval(showRestTime, 1000);
